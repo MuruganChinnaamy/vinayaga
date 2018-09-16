704432908b158d9734a555c8895a0336f97ca174
murugan <murugan@think42labs.com>
2018-09-16 05:48:25 +0530

    refs # employee changes

--- cms_employee/models/hr_employee.py
+++ cms_employee/models/hr_employee.py
@@ -13,7 +13,12 @@ import calendar
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
     
-    employee_id = fields.Char(string='Employee ID')
+    
+    
+    
+    employee_id = fields.Char(
+        'Employee ID', size=16, copy=False)
+        
     employee_type = fields.Selection([
         ('farmer', 'Farmer'),
         ('labour', 'Labour'),
@@ -22,7 +27,24 @@ class HrEmployee(models.Model):
     branch_id = fields.Many2one('res.company', 'Branch')
     village_id = fields.Many2one('village.master', 'Village')
     field_officers = fields.Many2many('hr.employee','employee_id_id','farmer_id','employee_employee_farmers_rel','Field Officers')
+    partner_id = fields.Many2one('res.partner', 'Partner')
     
+    @api.multi
+    def onchange_employee_type(self):
+        for record in self:
+            if record.employee_type == 'farmer':
+                record.employee_id = self.env['ir.sequence'].next_by_code('cms.farmer.new')
+            elif record.employee_type == 'labour':
+                record.employee_id = self.env['ir.sequence'].next_by_code('cms.labour.new')
+            else:
+                record.employee_id = self.env['ir.sequence'].next_by_code('cms.employee.new')
+                
+    @api.model
+    def create(self,vals):
+        res = super(HrEmployee, self).create(vals)
+        res.onchange_employee_type()
+        vals['employee_id'] = res.employee_id
+        return res    
 
 
 
