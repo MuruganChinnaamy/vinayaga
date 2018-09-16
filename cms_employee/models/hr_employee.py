a0f3a35f8b8b508d016da0982f5c16b1a383895e
murugan <murugan@think42labs.com>
2018-09-16 13:38:27 +0530

    refs # employee vs customers changes

--- cms_employee/models/hr_employee.py
+++ cms_employee/models/hr_employee.py
@@ -28,6 +28,7 @@ class HrEmployee(models.Model):
     village_id = fields.Many2one('village.master', 'Village')
     field_officers = fields.Many2many('hr.employee','employee_id_id','farmer_id','employee_employee_farmers_rel','Field Officers')
     partner_id = fields.Many2one('res.partner', 'Partner')
+    field_manager = fields.Boolean('Is a Field Manager')
     
     @api.multi
     def onchange_employee_type(self):
@@ -39,12 +40,41 @@ class HrEmployee(models.Model):
             else:
                 record.employee_id = self.env['ir.sequence'].next_by_code('cms.employee.new')
                 
+    @api.multi
+    def partner_create(self):
+        for record in self:
+            partner = self.env['res.partner'].create({
+                                            'company_type':'person',
+                                            'employee_id':record.id,
+                                            'employee_type':record.employee_type,
+                                            'name':record.name,
+                                            'image':record.image,
+                                            'company_id':record.branch_id.id
+                                            })
+            return partner
+        
+                
     @api.model
     def create(self,vals):
         res = super(HrEmployee, self).create(vals)
         res.onchange_employee_type()
         vals['employee_id'] = res.employee_id
-        return res    
+        partner = res.partner_create()
+        vals['partner_id'] = partner.id
+        return res  
+    
+    
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    employee_id = fields.Many2one('hr.employee', 'Employee')
+    employee_type = fields.Selection([
+        ('farmer', 'Farmer'),
+        ('labour', 'Labour'),
+        ('employee', 'Employee')], string='Employee Type')
+    
+    
+    
 
 
 
